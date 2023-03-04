64e783337b5413f303684f45c1272ef8869d4569
xtt <wtt150249@gmail.com>
2023-03-04 13:13:08 +0800

    添加路由懒加载

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,24 @@ export default defineConfig({
 	},
 	server: {
 		port: 8081
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					"test-group": [
+						"./src/pages/_test/_home.vue",
+						"./src/pages/_test/_icon.vue",
+						"./src/pages/_test/_button.vue",
+						"./src/pages/_test/_list.vue",
+						"./src/pages/_test/_linkbutton.vue",
+						"./src/pages/_test/_iconbutton.vue",
+						"./src/pages/_test/_alert.vue",
+						"./src/pages/_test/_card.vue",
+						"./src/pages/_test/_markdown.vue"
+					]
+				}
+			}
+		}
 	}
 });
