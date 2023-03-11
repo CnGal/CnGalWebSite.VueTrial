7e304dff470c24444da1552a417fbaffdf3cc2ff
xtt <wtt150249@gmail.com>
2023-03-11 13:49:26 +0800

    为 home 页面的请求数据添加缓存 #51

--- src/api/homeAPI/index.js
+++ src/api/homeAPI/index.js
@@ -1,39 +1,141 @@
 import axios from "../axios";
 import qs from "qs";
 
+const cache = new Map();
+
 async function getHomeCarouselsView() {
-	return await axios.get("/api/home/GetHomeCarouselsView");
+	// 如果cache中有数据，且未过期，则直接返回
+	if (cache.has("carousels")) {
+		const { value, expire } = cache.get("carousels");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+
+	const data = await axios.get("/api/home/GetHomeCarouselsView");
+	// 获取banner数据，将数据存入cache中，过期时间为5分钟
+	cache.set("carousels", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 5
+	});
+	return data;
 }
 
 async function getHomeNewsView() {
-	return await axios.get("/api/home/GetHomeNewsView");
+	if (cache.has("news")) {
+		const { value, expire } = cache.get("news");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+
+	const data = await axios.get("/api/home/GetHomeNewsView");
+	// 获取最近动态数据，将数据存入cache中，过期时间为3分钟
+	cache.set("news", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 3
+	});
+	return data;
 }
 
 async function getHomeRecentIssuelGameView() {
-	return await axios.get("/api/home/GetHomeRecentIssuelGameView");
+	if (cache.has("recentIssuelGame")) {
+		const { value, expire } = cache.get("recentIssuelGame");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetHomeRecentIssuelGameView");
+	// 获取近期新作数据，将数据存入cache中，过期时间为3分钟
+	cache.set("recentIssuelGame", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 3
+	});
+	return data;
 }
 
 async function getHomeNewestGameView() {
-	return await axios.get("/api/home/GetHomeNewestGameView");
+	if (cache.has("newestGame")) {
+		const { value, expire } = cache.get("newestGame");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetHomeNewestGameView");
+	// 获取即将发售数据，将数据存入cache中，过期时间为3分钟
+	cache.set("newestGame", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 3
+	});
+	return data;
 }
 
 async function getHomeArticlesView() {
-	return await axios.get("/api/home/GetHomeArticlesView");
+	if (cache.has("articles")) {
+		const { value, expire } = cache.get("articles");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetHomeArticlesView");
+	// 获取文章数据，将数据存入cache中，过期时间为3分钟
+	cache.set("articles", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 3
+	});
+	return data;
 }
 
 async function getHomeRecentEditView() {
-	return await axios.get("/api/home/GetHomeRecentEditView");
+	if (cache.has("recentEdit")) {
+		const { value, expire } = cache.get("recentEdit");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetHomeRecentEditView");
+	// 获取最近编辑数据，将数据存入cache中，过期时间为1分钟
+	cache.set("recentEdit", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 1
+	});
+	return data;
 }
 
 async function getHomeNoticesView() {
-	return await axios.get("/api/home/GetHomeNoticesView");
+	if (cache.has("notices")) {
+		const { value, expire } = cache.get("notices");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetHomeNoticesView");
+	// 获取公告数据，将数据存入cache中，过期时间为5分钟
+	cache.set("notices", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 5
+	});
+	return data;
 }
 
 async function getHomeFriendLinksView() {
-	return await axios.get("/api/home/GetHomeFriendLinksView");
+	if (cache.has("friendLinks")) {
+		const { value, expire } = cache.get("friendLinks");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetHomeFriendLinksView");
+	// 获取友情链接数据，将数据存入cache中，过期时间为5分钟
+	cache.set("friendLinks", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 5
+	});
+	return data;
 }
 
 async function getHomeSearch(params) {
+	// search 数据具有一定的时效性，不需要缓存
 	return await axios.get("/api/home/Search", {
 		params,
 		paramsSerializer: (params) =>
@@ -42,11 +144,35 @@ async function getHomeSearch(params) {
 }
 
 async function getHomeVideosView() {
-	return await axios.get("/api/home/GetHomeVideosView");
+	if (cache.has("videos")) {
+		const { value, expire } = cache.get("videos");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetHomeVideosView");
+	// 获取视频数据，将数据存入cache中，过期时间为3分钟
+	cache.set("videos", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 3
+	});
+	return data;
 }
 
 async function getDocuments() {
-	return await axios.get("/api/home/GetDocuments");
+	if (cache.has("documents")) {
+		const { value, expire } = cache.get("documents");
+		if (expire > Date.now()) {
+			return value;
+		}
+	}
+	const data = await axios.get("/api/home/GetDocuments");
+	// 获取文档数据，将数据存入cache中，过期时间为5分钟
+	cache.set("documents", {
+		value: data,
+		expire: Date.now() + 1000 * 60 * 5
+	});
+	return data;
 }
 
 export {
